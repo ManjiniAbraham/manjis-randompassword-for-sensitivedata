cefa78b8e8f255a834bc72d0c217ac7e2b51f873
manjini <manjini.abraham@gmail.com>
2023-06-19 21:00:05 -0400

    updated index.html and created an array for all characters

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,13 @@
+//create an array for all characters that can be chosen by the user.
+var uppercaseOptions = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','I','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercaseOptions = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numberOptions = ['1','2','3','4','5','6','7','8','9','0'];
+var specialCharactersOptions =['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
